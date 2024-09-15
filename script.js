2c4114a57bc950416d84bad18af41606f70b2293
David Kulp <dkulp2@users.noreply.github.com>
2024-09-15 23:47:32 +0000

    Added devcontainer with Live Preview, added Hello World, and created a pop-up in script.js

--- script.js
+++ script.js
@@ -5,11 +5,12 @@
 
 /* Var Alert */
 // make a string variable for a color, set it equal to your favorite
-// alert the message" "My favorite color is " plus the color
+var color="Blue"
 
 /* Var Alert Prompt */
 // make a string variable for color, prompt the user for their favorite
 // alert color " is a nice color!"
+alert("My favorite color is "+color)
 
 /* Var Alert Prompt Conditional */
 // make a variable for color, prompt the user for their favorite
